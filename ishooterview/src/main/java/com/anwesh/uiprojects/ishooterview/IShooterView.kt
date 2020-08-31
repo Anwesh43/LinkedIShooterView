68e17b88551413176a35c4bd0445dd46d5caca17
Anwesh43 <anweshthecool0@gmail.com>
2020-09-01 01:16:46 +0530

    adding constants and libraries

--- ishooterview/src/main/java/com/anwesh/uiprojects/ishooterview/IShooterView.kt
+++ ishooterview/src/main/java/com/anwesh/uiprojects/ishooterview/IShooterView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.ishooterview
 /**
  * Created by anweshmishra on 01/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#673AB7",
+        "#4CAF50",
+        "#FF9800",
+        "#03A9F4"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val barWFactor : Float = 6.5f
+val barHFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")

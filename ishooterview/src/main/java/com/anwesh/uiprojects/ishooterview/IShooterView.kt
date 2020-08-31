655a35885699e519500cd05e64d5d3506c13ae9e
Anwesh43 <anweshthecool0@gmail.com>
2020-09-01 01:43:42 +0530

    adding Animator class

--- ishooterview/src/main/java/com/anwesh/uiprojects/ishooterview/IShooterView.kt
+++ ishooterview/src/main/java/com/anwesh/uiprojects/ishooterview/IShooterView.kt
@@ -118,4 +118,45 @@ class IShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ISNode(var i : Int, val state : State = State()) {
+
+        private var next : ISNode? = null
+        private var prev : ISNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ISNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawISNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ISNode {
+            var curr : ISNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }

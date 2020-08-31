ce1f793647f5893d1c45e60feac5b4e39a3980e9
Anwesh43 <anweshthecool0@gmail.com>
2020-09-01 01:50:27 +0530

    addiun Renderer class

--- ishooterview/src/main/java/com/anwesh/uiprojects/ishooterview/IShooterView.kt
+++ ishooterview/src/main/java/com/anwesh/uiprojects/ishooterview/IShooterView.kt
@@ -182,4 +182,27 @@ class IShooterView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : IShooterView) {
+
+        private val animator : Animator = Animator(view)
+        private val ishooter : IShooter = IShooter(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ishooter.draw(canvas, paint)
+            animator.animate {
+                ishooter.update {
+                    animator.start()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ishooter.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
